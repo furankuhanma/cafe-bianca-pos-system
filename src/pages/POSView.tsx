ed7c688199b226c86f2d3d6fdba980e84ebfe979
unknown <laporefrankloui@gmail.com>
2025-12-17 09:52:19 +0800

    remove 3 dots

--- src/pages/POSView.tsx
+++ src/pages/POSView.tsx
@@ -139,11 +139,6 @@ export function POSView() {
                 <h1 className="font-bold text-gray-900">Cafe Bianca</h1>
               </div>
             </div>
-            <button className="p-2">
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
-              </svg>
-            </button>
           </div>
 
           {/* Search Bar */}
